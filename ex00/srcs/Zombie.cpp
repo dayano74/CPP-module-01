6d44b2bb7676c319de53786d584350dff954019a
dayano <kiixxn00@gmail.com>
2025-08-25 22:08:16 +0900

    add initial implementation of Zombie class and related functionality

---
+++ ex00/srcs/Zombie.cpp
@@ -0,0 +1,12 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(const std::string& name) : name(name) {
+}
+
+Zombie::~Zombie() {
+  std::cout << name << ": Destructed!" << std::endl;
+}
+
+void Zombie::announce(void) {
+  std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
