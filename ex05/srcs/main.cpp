495227b774fa1ec8c607174b974c63f977fb06c4
dayano74 <kiixxn00@gmail.com>
2025-09-04 14:07:12 +0900

    update

--- ex05/srcs/main.cpp
+++ ex05/srcs/main.cpp
@@ -1,12 +1,14 @@
 #include "../incs/Harl.hpp"
 
+#include <iostream>
+
 int main(void) {
   Harl harl;
 
-  harl.complain("DEBUG");
-  harl.complain("INFO");
-  harl.complain("WARNING");
-  harl.complain("ERROR");
+  harl.complain("DEBUG"); std::cout << std::endl;
+  harl.complain("INFO"); std::cout << std::endl;
+  harl.complain("WARNING"); std::cout << std::endl;
+  harl.complain("ERROR"); std::cout << std::endl;
 
   harl.complain("UNKNOWN");
   return 0;
